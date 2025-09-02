f9566e191650709d8bd8933ab4f34158e86fa55e
James Cook <cookjws@gmail.com>
2025-09-02 20:40:27 +0100

    remove unnecessary logic around integer orders

--- src/GeneralBesselj.jl
+++ src/GeneralBesselj.jl
@@ -10,18 +10,13 @@ const RTOL=1e-12
 const MAXITERS=2^10
 
 function hypergeom_0f1_fast(a::Number, z; atol=ATOL, rtol=RTOL, maxiters=MAXITERS)
-  T = promote_type(typeof(a), typeof(z))
+  T = float(promote_type(typeof(a), typeof(z)))
 
   (!(typeof(a) <: Dual) && isinteger(a)) && return T(SpecialFunctions.besselj(Int(a), z))
 
   # Handle special cases
   z == 0 && return one(T)
   
-  # For negative integer a, function is undefined
-  if isinteger(a) && a <= 0
-    return iseven(a) ? T(Inf) : T(-Inf)
-  end
-
   atol², rtol² = atol^2, rtol^2
   
   # Initialise variables with promoted types
@@ -40,27 +35,15 @@ function hypergeom_0f1_fast(a::Number, z; atol=ATOL, rtol=RTOL, maxiters=MAXITER
 end
 
 function hypergeom_0f1_fast(a::AbstractVector, z; atol=ATOL, rtol=RTOL, maxiters=MAXITERS)
-  T = promote_type(eltype(a), typeof(z))
+  T = float(promote_type(eltype(a), typeof(z)))
   n = length(a)
 
-  (!(eltype(a) <: Dual) && all(isinteger, a)) && return T(SpecialFunctions.besselj.(Int.(a), z))
-
-  if any(i->isinteger(i) && i <= 0, a)
-    throw(ErrorException("All orders must be positive if they are integers"))
-  end
+  (!(eltype(a) <: Dual) && all(isinteger, a)) && return T.(SpecialFunctions.besselj.(Int.(a), z))
 
   # Handle special cases
   sum_val = similar(a, T)
   fill!(sum_val, 1)
   z == 0 && return sum_val
-
-  # For negative integer a, function is undefined
-  if all(isinteger, a) && all(i->i<=0, a)
-    for (i, ai) in enumerate(a)
-      sum_val[i] = iseven(ai) ? T(Inf) : T(-Inf)
-    end
-    return sum_val
-  end
 
   atol², rtol² = atol^2, rtol^2
 
@@ -91,13 +74,13 @@ function _factor(a, z)
   end
 end
 function besselj(a, z; rtol=RTOL, atol=ATOL, maxiters=MAXITERS)
-  T = promote_type(typeof(a), typeof(z))
+  T = float(promote_type(typeof(a), typeof(z)))
   return T(_factor(a, z) * hypergeom_0f1_fast(a + 1, -z^2 / 4;
     atol=atol, rtol=rtol, maxiters=maxiters))
 end
 function besselj_v(a::AbstractVector{T}, z; rtol=RTOL, atol=ATOL, maxiters=MAXITERS
     ) where {T}
-  U = promote_type(eltype(a), typeof(z))
+  U = float(promote_type(eltype(a), typeof(z)))
   return U.(_factor.(a, z)) .* hypergeom_0f1_fast(a .+ 1, -z^2 / 4;
     atol=atol, rtol=rtol, maxiters=maxiters)
 end
